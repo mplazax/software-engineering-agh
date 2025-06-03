7e8924193e92e1c5d935b0bbe8d0ea8fed65fd53
Michał Plaza <michalplaza9@gmail.com>
2025-06-03 14:26:21 +0200

    chore: update mock data, add exception handling and refactor file locations

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -2,22 +2,32 @@ from sqlalchemy import create_engine, text
 from model import Base
 from example_data import add_data
 
-DATABASE_URL = "postgresql+psycopg2://admin:admin@localhost:5433/database"
+def main():
 
-engine = create_engine(DATABASE_URL)
+    DATABASE_URL = "postgresql+psycopg2://admin:admin@localhost:5433/database"
 
-with engine.connect() as conn:
-    conn.execute(text("DROP TABLE IF EXISTS availability_proposals CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS change_recommendations CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS change_requests CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS course_events CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS courses CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS room_unavailability CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS rooms CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS users CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS groups CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS time_slots CASCADE"))
-    conn.execute(text("DROP TYPE IF EXISTS userrole CASCADE"))
-    conn.commit()
-    Base.metadata.create_all(bind=engine)
-    add_data()
+    engine = create_engine(DATABASE_URL)
+
+    with engine.connect() as conn:
+        conn.execute(text("DROP TABLE IF EXISTS availability_proposals CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS change_recommendations CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS change_requests CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS course_events CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS courses CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS room_unavailability CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS rooms CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS users CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS groups CASCADE"))
+        conn.execute(text("DROP TABLE IF EXISTS time_slots CASCADE"))
+        conn.execute(text("DROP TYPE IF EXISTS userrole CASCADE"))
+        conn.commit()
+        Base.metadata.create_all(bind=engine)
+        add_data(DATABASE_URL)
+
+if __name__ == "__main__":
+    decision = input("Executing this script will erase all current data, Y to proceed: ")
+    if decision in ['y', 'Y']:
+        print("Proceeding...")
+        main()
+    else:
+        print("Execution stopped")
